66d52068c3ef3e50876c53267f9d5489c2d9f562
abhinav-max34 <lalitapanwar0348@gmail.com>
2025-12-31 12:24:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-// Replace with your own OpenWeatherMap API key
 let apiKey = "fdb8124c26cdbfb5df776b72294d945d";
 
 function getWeather() {
-    let city = document.getElementById("city").value;
+    let city = document.getElementById("city").value.trim();
 
-    let link = "https://api.openweathermap.org/data/2.5/weather?q=" 
-               + city + "&appid=" + apiKey + "&units=metric";
+    if (city === "") {
+        document.getElementById("result").innerHTML = "Please enter a city name";
+        return;
+    }
 
-    fetch(link)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("result").innerHTML =
-        "City: " + city + "<br>" +
-        "Temperature: " + data.main.temp + " C<br>" +
-        "Weather: " + data.weather[0].main + "<br>" +
-        "Humidity: " + data.main.humidity + "%<br>" +
-        "Wind Speed: " + data.wind.speed + " m/s<br>" +
-        "<img src='http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png'>";
-    })
-    .catch(() => {
-        document.getElementById("result").innerHTML = "City not found";
-    });
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            if (data.cod !== 200) {
+                document.getElementById("result").innerHTML = "City not found";
+                return;
+            }
+
+            document.getElementById("result").innerHTML =
+                "City: " + data.name + "<br>" +
+                "Temperature: " + data.main.temp + " °C<br>" +
+                "Weather: " + data.weather[0].main + "<br>" +
+                "Humidity: " + data.main.humidity + "%<br>" +
+                "<img src='https://openweathermap.org/img/wn/" +
+                data.weather[0].icon + "@2x.png'>";
+        })
+        .catch(() => {
+            document.getElementById("result").innerHTML = "Error loading data";
+        });
 }
 
-// Show sample 1-month temperature
-function showMonthlyTemp() {
-    document.getElementById("monthly").innerHTML =
-    "<strong>Average Temperature (Last 30 Days)</strong><br>" +
-    "Week 1: 28°C<br>" +
-    "Week 2: 30°C<br>" +
-    "Week 3: 32°C<br>" +
-    "Week 4: 31°C";
-}
-function getWeather() {
-Weather App
-
-[ Enter city name ]
-
-[ Show Weather ]
-
-Temperature: ...
 
 
 
